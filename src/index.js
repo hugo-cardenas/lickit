c9ce7006a991dc779e1a22f5e70fa35e0c4f2dc4
hugo <hcgar00@gmail.com>
2017-06-27 16:39:13 +0200

    Remove own stateStorage, use instead electron-json-storage

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import _ from 'lodash';
+// import './index.css'; 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './render/App';
+import _ from 'lodash';
+import jsonStorage from 'electron-json-storage';
+import pify from 'pify';
 import registerServiceWorker from './registerServiceWorker';
-// import './index.css'; 
-import {Provider} from 'react-redux'; 
+import { Provider } from 'react-redux';
+import App from './render/App';
 import createStore from './state/store';
-// import storage from './state/storage';
+
+const storage = pify(jsonStorage);
 
 const link = "http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg";
 
 function rand(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 const descriptions = [
-  'Django - Lady be good\nhttps://youtu.be/2Am48nza7JE?t=16s', 
-  'Django - Blues clair', 
-  'Django - Blues clair 2', 
-  'Charlie Parker - Confirmation', 
+  'Django - Lady be good\nhttps://youtu.be/2Am48nza7JE?t=16s',
+  'Django - Blues clair',
+  'Django - Blues clair 2',
+  'Charlie Parker - Confirmation',
   'Dizzy Gillespie - Perdido\nhttps://youtu.be/X8gCmtkuVgk?t=20s'
 ];
 
@@ -34,46 +37,36 @@ const tags = [
 ]
 
 function getInitialState() {
-  const licks = _
-    .range(2)
-    .map(i => {
-      return {
-        id: i + 1,
-        description: descriptions[rand(0, descriptions.length - 1)],
-        tracks: _.range(rand(0, 2)).map(i => {return {
-          id: i,
-          link: link + `?a=${i}`
-        }}),
-        tags: _.uniq(_.range(rand(0, 3)).map(i => tags[rand(0, tags.length - 1)]))
-      };
-    });
+    const licks = _
+        .range(2)
+        .map(i => {
+            return {
+                id: i + 1,
+                description: descriptions[rand(0, descriptions.length - 1)],
+                tracks: _.range(rand(0, 2)).map(i => {
+                    return {
+                        id: i,
+                        link: link + `?a=${i}`
+                    }
+                }),
+                tags: _.uniq(_.range(rand(0, 3)).map(i => tags[rand(0, tags.length - 1)]))
+            };
+        });
 
-  return {licks: licks.map(lick => { return {lick}; })};
+    return { licks: licks.map(lick => { return { lick }; }) };
 }
 
 // TODO Handle errors
-const initialState = getInitialState();
-const store = createStore(initialState);
-ReactDOM.render(
-  <Provider store={store}>
-  <App/>
-</Provider>, document.getElementById('root'));
 
-// registerServiceWorker();
-  
+storage.get('state')
+    .then(initialState => {
+        const store = createStore(initialState);
+        store.subscribe(() => {
+            storage.set('state', store.getState());
+        });
 
-// storage.loadState()
-//   .then(initialState => {
-//     const store = createStore(initialState);
-//     store.subscribe(() => {
-//       console.log('SAVING STATE', store.getState());
-//       storage.saveState(store.getState());
-//     });
-
-//     ReactDOM.render(
-//       <Provider store={store}>
-//       <App/>
-//     </Provider>, document.getElementById('root'));
-
-//     registerServiceWorker();
-//   });
+        ReactDOM.render(
+            <Provider store={store}>
+        <App/>
+        </Provider>, document.getElementById('root'));
+    });
--- src/state/storage/index.js
+++
@@ -1,8 +0,0 @@
-import createStorage from './stateStorage';
-
-// TODO Import config from central locator
-const config = {
-    filePath: '/tmp/foo.json'
-};
-
-export default createStorage(config);
