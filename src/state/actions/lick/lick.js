41332bc6b0c7bf2ed9ad1563d019a9c427d24fad
hugo <hcgar00@gmail.com>
2017-07-09 22:18:26 +0200

    Add track file deletion usage to updateLick redux action

--- src/state/actions/lick/lick.js
+++ src/state/actions/lick/lick.js
@@ -9,29 +9,39 @@ export default function getActions(trackStorage) {
         return { type: LICK_CREATE };
     }
 
-    // Redux-thunk async action (save tracks to filesystem)
+    // Redux-thunk async action (save/delete tracks to filesystem)
     function updateLick(lick) {
-        return (dispatch) => {
+        return async(dispatch, getState) => {
             // TODO Validate lick here? - NO. Validate only the required part for saving tracks
 
-            // TODO Filter also tracks missing from the new state and delete them from filesystem
-            // TODO Implement storage.deleteTrack(id)
-            const tracks = lick.tracks;
-            return Promise.all(tracks.map(track => saveTrack(track)))
-                .then(tracks => {
-                    return dispatch({
-                        type: LICK_UPDATE,
-                        lick: Object.assign({}, lick, { tracks })
-                    });
-                })
-                .catch(error => {
-                    // TODO Handle error
-                    console.error(error);
-                });
+            // TODO Handle what to do if only some of the promises fail 
+            // (e.g. deletes successfully all tracks except one)
+
+            // TODO May write a Redux selector for this http://redux.js.org/docs/recipes/ComputingDerivedData.html
+
+            const storedTracks = getState().licks
+                .find(lickState => lickState.lick.id === lick.id)
+                .lick.tracks;
+
+            let tracks;
+            try {
+                // Handle all new tracks submitted in form
+                tracks = await Promise.all(lick.tracks.map(track => handleTrack(track)));
+                // Delete all stored tracks which haven't been submitted
+                const ids = tracks.map(track => track.id);
+                const toBeDeletedTracks = storedTracks.filter(track => !ids.includes(track.id));
+                await Promise.all(toBeDeletedTracks.map(track => deleteTrack(track.id)));
+            } catch (error) {
+                throw error;
+            }
+            return dispatch({
+                type: LICK_UPDATE,
+                lick: {...lick, tracks}
+            });
         };
     }
 
-    async function saveTrack(track) {
+    async function handleTrack(track) {
         if (track.blob) {
             // Save new track
             const id = await trackStorage.saveBlob(track.blob);
@@ -41,9 +51,13 @@ export default function getActions(trackStorage) {
         return track;
     }
 
+    async function deleteTrack(id) {
+        trackStorage.deleteTrack(id);
+    }
+
     function deleteLick(id) {
         return { type: LICK_DELETE, id };
     }
 
     return { createLick, updateLick, deleteLick };
-};
+}
--- test/state/actions/lick.test.js
+++ test/state/actions/lick.test.js
@@ -4,43 +4,72 @@ import getActions from 'src/state/actions/lick/lick';
 // TODO Test error
 
 it('update lick', async() => {
+    const state = {
+        licks: [
+            { lick: { id: 512 } },
+            {
+                lick: {
+                    id: 1024,
+                    tracks: [
+                        { id: 40 }, // To be deleted
+                        { id: 42 },
+                        { id: 44 }, // To be deleted
+                        { id: 46 },
+                    ]
+                }
+            },
+            { lick: { id: 2048 } }
+        ]
+    };
+
     const blob1 = new Blob(['foo']);
     const blob2 = new Blob(['bar']);
 
     const lick = {
+        id: 1024,
         tracks: [
             { id: 42 },
-            { blob: blob1 },
-            { id: 44 },
-            { blob: blob2 },
+            { blob: blob1 }, // To be created
+            { id: 46 },
+            { blob: blob2 }, // To be created
         ]
     };
 
-    const storage = { saveBlob: jest.fn() };
-    storage.saveBlob.mockReturnValueOnce(100);
-    storage.saveBlob.mockReturnValueOnce(200);
-
+    const storage = {
+        saveBlob: jest.fn()
+            .mockReturnValueOnce(Promise.resolve(100))
+            .mockReturnValueOnce(Promise.resolve(200)),
+        deleteTrack: jest.fn()
+            .mockReturnValueOnce(Promise.resolve())
+            .mockReturnValueOnce(Promise.resolve())
+    };
     const { updateLick } = getActions(storage);
 
     const action = updateLick(lick);
     const dispatch = jest.fn();
-    await action(dispatch);
-
-    expect(storage.saveBlob.mock.calls.length).toBe(2);
-    expect(storage.saveBlob.mock.calls[0][0]).toEqual(blob1);
-    expect(storage.saveBlob.mock.calls[1][0]).toEqual(blob2);
+    const getState = jest.fn().mockReturnValueOnce(state);
+    await action(dispatch, getState);
 
     const expectedLick = {
+        id: 1024,
         tracks: [
             { id: 42 },
             { id: 100 },
-            { id: 44 },
-            { id: 200 },
+            { id: 46 },
+            { id: 200 }
         ]
     };
 
-    expect(dispatch.mock.calls.length).toBe(1);
-    expect(dispatch.mock.calls[0][0]).toEqual({
+    expect(storage.saveBlob).toHaveBeenCalledTimes(2);
+    expect(storage.saveBlob).toHaveBeenCalledWith(blob1);
+    expect(storage.saveBlob).toHaveBeenCalledWith(blob2);
+
+    expect(storage.deleteTrack).toHaveBeenCalledTimes(2);
+    expect(storage.deleteTrack).toHaveBeenCalledWith(40);
+    expect(storage.deleteTrack).toHaveBeenCalledWith(44);
+
+    expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toHaveBeenCalledWith({
         type: LICK_UPDATE,
         lick: expectedLick
     });
