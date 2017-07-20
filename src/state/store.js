9124345df6f9ce1e01e66c42d04c604bde96cb2a
hugo <hcgar00@gmail.com>
2017-07-20 18:53:59 +0200

    Implement error middleware and apply it on the store

--- src/state/store.js
+++ src/state/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import errorMiddleware from './middleware/error';
 
-export default(initialState) => {
-    return createStore(rootReducer, initialState, applyMiddleware(thunk));
-}
+export default (initialState) => {
+    return createStore(rootReducer, initialState, applyMiddleware(errorMiddleware, thunk));
+};
