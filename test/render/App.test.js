0f69728e07dda013dd45387b244a7ce1dac2f898
hugo <hcgar00@gmail.com>
2017-08-07 17:49:22 +0200

    Add test for App component looping through a number of lick items

--- test/render/App.test.js
+++ test/render/App.test.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from 'src/render/App';
 import createStore from 'src/state/store';
+import { range } from 'lodash';
+import { shallow } from 'enzyme';
 
 // Mock the call electron.app.getPath('userData')
 jest.mock('electron', () => {
@@ -39,6 +41,24 @@ it('renders without crashing', () => {
         <Provider store={store}>
       <App {...getProps()}/>
   </Provider>, div);
+});
+
+it('renders expected number of licks', () => {
+    const props = getProps();
+    props.licks = range(7).map(i => {
+        return {
+            lick: {
+                id: 'c' + i,
+                description: '',
+                tracks: [],
+                tags: []
+            }
+        };
+    });
+    
+    const component = shallow(<App {...props}/>);
+    expect(component.find('.columns')).toHaveLength(3);
+    expect(component.find('Lick')).toHaveLength(7);
 });
 
 function getProps() {
